b4ffe5004ac7871e2a1bddc1750e8d33a3764e37
fxnoob <fxnoob71@gmail.com>
2019-01-17 21:24:33 +0530

    popup page setup

---
+++ popup-page/components/helloworld.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class HelloWorld extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+
+        return (
+            <div>HelloWorld</div>
+        );
+    }
+
+}
