8ea077832a24bca800fc1ce877e609765cd8f468
fxnoob <fxnoob71@gmail.com>
2019-01-19 18:26:01 +0530

    google oauth

---
+++ src/utils/oauth.js
@@ -0,0 +1,19 @@
+
+export default class oauth {
+    constructor(){};
+    /*
+    * get access token from google oauth
+    * */
+    getToken() {
+        return new Promise((resolve, reject) => {
+           try {
+               chrome.identity.getAuthToken({interactive: true}, function(token) {
+                   resolve(token);
+               });
+           }
+           catch (e) {
+               reject(e);
+           }
+        });
+    }
+}
